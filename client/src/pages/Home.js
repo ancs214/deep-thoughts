5f8134c067825faa0400aac51f944d64989a01df
ancs214 <ancs214@gmail.com>
2022-06-26 12:02:36 -0400

    added logged in user based queries to personalize homepage and profile page if user is logged in

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
+import Auth from '../utils/auth';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
+import FriendList from '../components/FriendList';
+
 
 const Home = () => {
   // use useQuery hook to make query request
   //Apollo's @apollo/client library provides a loading property to indicate that the request isn't done just yet. When it's finished and we have data returned from the server, that information is stored in the destructured data property.
   const { loading, data } = useQuery(QUERY_THOUGHTS);
+  // use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive
+  //userData will hold all returned info from the query
+  const { data: userData } = useQuery(QUERY_ME_BASIC);
+
 
   //NEW SYNTAX: optional chaining   -   negates the need to check if an obj even exists before accessing its properties
   //if data exists, store it in the thoughts constant we just created. if data is undefined, save an empty array to the thoughts component
   const thoughts = data?.thoughts || [];
   // console.log(thoughts);
 
+  const loggedIn = Auth.loggedIn();
+
   //If the query hasn't completed and loading is still defined, we display a message to indicate just that. Once the query is complete and loading is undefined, we pass the thoughts array and a custom title to the <ThoughtList> component as props.
   return (
     <main>
       <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>
+        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
             <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
           )}
+          {loggedIn && userData ? (
+            <div className="col-12 col-lg-3 mb-3">
+              <FriendList
+                username={userData.me.username}
+                friendCount={userData.me.friendCount}
+                friends={userData.me.friends}
+              />
+            </div>
+          ) : null}
         </div>
       </div>
     </main>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
@@ -9,21 +10,37 @@ const Profile = () => {
   //useParams will retrieve the username from the URL, which is then passed to useQuery hook
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, { 
+  //if there's a value in userParam that we got from the URL bar, we'll use that value to run the QUERY_USER query. If there's no value in userParam, like if we simply visit /profile as a logged-in user, we'll execute the QUERY_ME query instead.
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, { 
     variables: { username: userParam }
   });
 
-  const user = data?.user || {};
+  //when we run QUERY_ME, response will return with data in the me property; but if it runs QUERY_USER , response will return with data in the user property.
+  const user = data?.me || data?.user || {};
+
+  //navigate to personal profile page if username is the logged-in username
+  //check to see if user is logged in and if username from JWT is same as userParam value. if a match, return to profile page
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to='/profile' />
+  }
 
   if (loading) {
     return <div>Loading...</div>
   }
 
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+      </h4>
+    );
+  }  
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {user.username}'s profile.
+          Viewing { userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
       </div>
 
