01447fd4fc6259942bca27231628bb34263d77fe
ancs214 <ancs214@gmail.com>
2022-06-26 13:20:22 -0400

    add friend functionality complete

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 import Auth from '../utils/auth';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
 const Profile = () => {
+  const [addFriend] = useMutation(ADD_FRIEND);
+
   //useParams will retrieve the username from the URL, which is then passed to useQuery hook
   const { username: userParam } = useParams();
 
   //if there's a value in userParam that we got from the URL bar, we'll use that value to run the QUERY_USER query. If there's no value in userParam, like if we simply visit /profile as a logged-in user, we'll execute the QUERY_ME query instead.
-  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, { 
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
 
@@ -34,14 +37,30 @@ const Profile = () => {
         You need to be logged in to see this page. Use the navigation links above to sign up or log in!
       </h4>
     );
-  }  
+  }
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing { userParam ? `${user.username}'s` : 'your'} profile.
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
+
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -50,11 +69,11 @@ const Profile = () => {
         </div>
 
         <div className="col-12 col-lg-3 mb-3">
-        <FriendList
-          username={user.username}
-          friendCount={user.friendCount}
-          friends={user.friends}
-        />
+          <FriendList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          />
         </div>
       </div>
     </div>
